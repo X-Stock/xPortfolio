fdee90615d1a9699a887e9cb746f596b8cbb9582
Kim Minh <100175752+kim-minh@users.noreply.github.com>
2025-02-17 22:18:39 +0700

    Add env example

--- src/xPortfolio/utils/api_client.py
+++ src/xPortfolio/utils/api_client.py
@@ -8,9 +8,9 @@ import xstockapi
 def api_client():
     client_id = os.getenv('CLIENT_ID')
     client_secret = os.getenv('CLIENT_SECRET')
-    api_host = os.getenv('API_HOST')
+    api_endpoint = os.getenv('API_ENDPOINT')
 
-    client = xstockapi.Client(client_id, client_secret, api_host)
+    client = xstockapi.Client(client_id, client_secret, api_endpoint)
     try:
         yield client
     finally:
